e229ecf6dc3e94d2f797855b54f0b74ae6665701
Ahmed Ashraf <ahmed.ashraf.saad@gmail.com>
2024-03-22 15:42:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,196 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+
+  const handleDeleteItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+
+  const handleToggleItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
+  const handleClearList = () => {
+    // If user clicks OK = true
+    // If user clicks CANCEL = flase
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) {
+      setItems([]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form({ onAddItems }) {
+  const [phrase, setPhrase] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Condition to STOP printing a new item into the list in case user haven't included an item in input field!
+    if (!phrase) return;
+
+    // Once form is submitted, we will create a new item object based on the pieces of states defined above!
+    const newItem = { phrase, quantity, packed: false, id: Date.now() };
+    onAddItems(newItem);
+
+    setPhrase("");
+    setQuantity(1);
+  };
+
+  const handleInputChange = (e) => {
+    setPhrase(e.target.value);
+  };
+
+  const handleSelectChange = (e) => {
+    setQuantity(Number(e.target.value));
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select value={quantity} onChange={handleSelectChange}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={phrase}
+        onChange={handleInputChange}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.phrase.localeCompare(b.phrase));
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
+  const handleChange = (e) => {
+    setSortBy(e.target.value);
+  };
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            itemObject={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={handleChange}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
+    </div>
+  );
+}
+
+function Item({ itemObject, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={itemObject.packed}
+        onChange={() => onToggleItem(itemObject.id)}
+      />
+      <span style={itemObject.packed ? { textDecoration: "line-through" } : {}}>
+        {itemObject.quantity} {itemObject.phrase}
+      </span>
+      <button onClick={() => onDeleteItem(itemObject.id)}>❌</button>
+    </li>
+  );
+}
+
+function Stats({ items }) {
+  // If there are no elements anyway in the array, then don't even bother with all of code below! Using early return technique
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list! 🚀</em>
+      </p>
+    );
+  // For the stats in the footer. Derived from the above state! No need to create a new state!
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      {percentage === 100 ? (
+        <em>You got everything! Ready to go ✈</em>
+      ) : (
+        <em>
+          💼 You have {numItems} items on your list, and you already packed{" "}
+          {numPacked} ({percentage}%)
+        </em>
+      )}
+    </footer>
+  );
+}
